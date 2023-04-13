94a91eea4a1475b42606e5bc2dc9a74fc63edb7d
Natalia <114990082+shanatta@users.noreply.github.com>
2023-04-13 14:49:50 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,3 +37,19 @@ var swiper = new Swiper(".popular-content", {
       },
     },
 }); 
+// Show Video
+let playButton = document.querySelector(".play-movie");
+let video = document.querySelector(".video-container");
+let myvideo = document.querySelector("#myvideo");
+let closebtn = document.querySelector(".close-video");
+
+playButton.onclick = () => {
+  video.classList.add("show-video");
+  // Auto Play When Click on Button
+  myvideo.play();
+};
+closebtn.onclick = () => {
+  video.classList.remove("show-video");
+  // Pause on Close Video
+  myvideo.pause();
+}; 
